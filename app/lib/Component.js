e1b70e2e0a314e4998dc662f43b3ace68ad9a49d
Xhesi Balla <xhesiballa15@gmail.com>
2019-05-08 14:46:17 +0200

    Deep proxify the model

--- app/lib/Component.js
+++ app/lib/Component.js
@@ -1,12 +1,12 @@
 import Mustache from 'mustache/mustache.js';
+import * as DeepProxy from './DeepProxy.js';
 
 export default class Component{
 	constructor() {
-		console.log('Component');
 		this.childComponents = [];
 		let symbol = Symbol.for("model");
 		this[symbol] = {}
-		this.model = new Proxy(this[symbol], this);
+		this.model = DeepProxy.create(this[symbol], this)
 	}
 
 	render(){
@@ -15,12 +15,6 @@ export default class Component{
 	}
 
 	set(obj, prop, value){
-		let model = this[Symbol.for("model")];
-		console.debug(model);
-		console.debug(` prop: ${prop}, value: ${value}`);
-		obj[prop] = value;
-		console.debug(model);
 		this.render();
-		return true;
 	}
 }
