19d75266f9523d6ebf69d0f4e2b0e780611ba7de
Xhesi Balla <xhesiballa15@gmail.com>
2019-05-14 01:57:38 +0200

    Initialise subcomponents

--- app/lib/Component.js
+++ app/lib/Component.js
@@ -3,14 +3,28 @@ import * as DeepProxy from './DeepProxy.js';
 
 export default class Component{
 	constructor() {
-		this.childComponents = [];
+		Object.assign(this, this.getConfig());
+
 		let symbol = Symbol.for("model");
 		this[symbol] = {}
-		this.model = DeepProxy.create(this[symbol], this)
+		this.model = DeepProxy.create(this[symbol], this);
+		this.render();
+		this.initialiseSubComponents();
+	}
+
+	initialiseSubComponents(){
+		if(this.components){
+			this.components.forEach((promise) => {
+				promise.then(module => new module.default());
+			});
+		}
 	}
 
 	render(){
 		let model = this[Symbol.for("model")];
+		if(typeof this.selector == 'string'){
+			this.selector = document.getElementsByTagName(this.selector)[0];
+		}
 		this.selector.innerHTML = Mustache.render(this.template, model);
 	}
 
